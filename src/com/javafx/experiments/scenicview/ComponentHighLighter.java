21d2e663f3c6d0f1d4f24370b1cbab8527e6724c
anderruiz <devnull@localhost>
2012-06-20 16:24:21 +0200

    [BUG] Fixed the highlighting of Text nodes

--- src/com/javafx/experiments/scenicview/ComponentHighLighter.java
+++ src/com/javafx/experiments/scenicview/ComponentHighLighter.java
@@ -1,6 +1,6 @@
 package com.javafx.experiments.scenicview;
 
-import javafx.geometry.*;
+import javafx.geometry.Bounds;
 import javafx.scene.Group;
 import javafx.scene.control.TitledPane;
 import javafx.scene.paint.Color;
@@ -9,14 +9,14 @@ import javafx.scene.text.Text;
 
 public class ComponentHighLighter extends Group {
 
-    public ComponentHighLighter(final NodeInfo node, final double width, final double height, final Bounds bounds, final Point2D start) {
+    public ComponentHighLighter(final NodeInfo node, final double width, final double height, final Bounds bounds) {
         if(width == -1) {
             final Rectangle rect = new Rectangle();
             rect.setFill(Color.TRANSPARENT);
             rect.setStroke(Color.ORANGE);
             rect.setMouseTransparent(true);
-            rect.setLayoutX(start.getX());
-            rect.setLayoutY(start.getY());
+            rect.setLayoutX(bounds.getMinX());
+            rect.setLayoutY(bounds.getMinY());
             rect.setStrokeWidth(3);
             rect.setWidth(bounds.getMaxX() - bounds.getMinX());
             rect.setHeight(bounds.getMaxY() - bounds.getMinY());
@@ -25,8 +25,8 @@ public class ComponentHighLighter extends Group {
         else {
             final Rectangle base = new Rectangle(width, height);
             final Rectangle rect = new Rectangle();
-            rect.setLayoutX(start.getX());
-            rect.setLayoutY(start.getY());
+            rect.setLayoutX(bounds.getMinX());
+            rect.setLayoutY(bounds.getMinY());
             rect.setStrokeWidth(3);
             rect.setWidth(bounds.getMaxX() - bounds.getMinX());
             rect.setHeight(bounds.getMaxY() - bounds.getMinY());
@@ -41,17 +41,22 @@ public class ComponentHighLighter extends Group {
             pane.setPrefHeight(60);
             pane.setPrefWidth(100);
             final Text label = new Text();
-            label.setText("x:"+DisplayUtils.format(start.getX())+" y:"+DisplayUtils.format(start.getY())+"\nw:"+DisplayUtils.format(rect.getWidth())+" h:"+DisplayUtils.format(rect.getHeight()));
+            label.setText("x:"+DisplayUtils.format(bounds.getMinX())+" y:"+DisplayUtils.format(bounds.getMinY())+"\nw:"+DisplayUtils.format(rect.getWidth())+" h:"+DisplayUtils.format(rect.getHeight()));
             pane.setContent(label);
-            pane.setLayoutX(start.getX()+(rect.getWidth()/2)-(pane.getPrefWidth()/2));
+            pane.setLayoutX(bounds.getMinX()+(rect.getWidth()/2)-(pane.getPrefWidth()/2));
+            pane.setFocusTraversable(false);
+            
             if(pane.getLayoutX()<0) {
                 pane.setLayoutX(0);
             }
-            if(start.getY()-pane.getPrefHeight()>=0) {
-                pane.setLayoutY(start.getY()-pane.getPrefHeight());
+            else if(pane.getLayoutX()+pane.getPrefWidth()>=width){
+                pane.setLayoutX(width-pane.getPrefWidth());
             }
-            else if(start.getY()+rect.getHeight()+pane.getPrefHeight()<=height) {
-                pane.setLayoutY(start.getY()+rect.getHeight());
+            if(bounds.getMinY()-pane.getPrefHeight()>=0) {
+                pane.setLayoutY(bounds.getMinY()-pane.getPrefHeight());
+            }
+            else if(bounds.getMinY()+rect.getHeight()+pane.getPrefHeight()<=height) {
+                pane.setLayoutY(bounds.getMinY()+rect.getHeight());
             }
             else {
                 pane.setLayoutY(0);
