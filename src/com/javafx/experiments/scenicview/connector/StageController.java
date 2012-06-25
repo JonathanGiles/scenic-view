2305b713e2a6bc7130ae67f9ac2782bdcb428f9b
anderruiz <devnull@localhost>
2012-06-25 11:53:07 +0200

    New interface StageController

---
+++ src/com/javafx/experiments/scenicview/connector/StageController.java
@@ -0,0 +1,22 @@
+package com.javafx.experiments.scenicview.connector;
+
+import com.javafx.experiments.scenicview.connector.event.AppEventDispatcher;
+import com.javafx.experiments.scenicview.connector.node.SVNode;
+
+public interface StageController {
+
+    public static final String SCENIC_VIEW_BASE_ID = "ScenicView.";
+
+    StageID getID();
+
+    void update();
+
+    void configurationUpdated(Configuration configuration);
+
+    void close();
+
+    void setEventDispatcher(AppEventDispatcher stageModelListener);
+
+    void setSelectedNode(SVNode value);
+
+}
