748827c432b7870e8f782c42fb4df105626c7adb
anderruiz <devnull@localhost>
2012-06-25 11:34:13 +0200

    RMI Code Cleanup

--- src/com/javafx/experiments/scenicview/remote/RemoteApplication.java
+++
@@ -1,9 +0,0 @@
-package com.javafx.experiments.scenicview.remote;
-
-import java.rmi.*;
-
-public interface RemoteApplication extends Remote {
-
-    public void sendInfo(String info) throws RemoteException;
-
-}
