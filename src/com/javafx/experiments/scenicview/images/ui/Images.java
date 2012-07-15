d6aa55984a40232d30aa0894b392cfa7ff24d508
Jonathan Giles <jonathan@jonathangiles.net>
2012-07-15 16:01:32 +1200

    Improved startup support for jfxrt.jar and tools.jar discovery, although jfxrt.jar still has no intelligence to discover the file.

---
+++ src/com/javafx/experiments/scenicview/images/ui/Images.java
@@ -0,0 +1,6 @@
+package com.javafx.experiments.scenicview.images.ui;
+
+/**
+ * Used for an easier reference to the image files in this package
+ */
+public class Images { }
---
+++ src/com/javafx/experiments/scenicview/utils/PathChangeListener.java
@@ -0,0 +1,13 @@
+package com.javafx.experiments.scenicview.utils;
+
+import java.util.Map;
+
+/**
+ *
+ */
+public interface PathChangeListener {
+    public static final String TOOLS_JAR_KEY = "tools.jar";
+    public static final String JFXRT_JAR_KEY = "jfxrt.jar";
+
+    public void onPathChanged(Map<String, String> map);
+}
