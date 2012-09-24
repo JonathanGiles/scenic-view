d8ebde5f502778f92f51faf961a52dd0031342f9
anderruiz <devnull@localhost>
2012-09-24 14:15:47 +0200

    Improve the reusability of InfoBox dialog

--- src/com/javafx/experiments/scenicview/dialog/InfoBox.java
+++ src/com/javafx/experiments/scenicview/dialog/InfoBox.java
@@ -11,11 +11,9 @@ import com.javafx.experiments.scenicview.ScenicView;
 
 public class InfoBox {
 
-    public InfoBox(final String title, final String labelText, final String textAreaText) {
-        final int width = 700;
-        final int height = 600;
+    public InfoBox(final String title, final String labelText, final String textAreaText, final int width, final int height) {
         final VBox pane = new VBox();
-        final Scene scene = SceneBuilder.create().width(600).height(400).root(pane).stylesheets(ScenicView.STYLESHEETS).build();
+        final Scene scene = SceneBuilder.create().width(width).height(height).root(pane).stylesheets(ScenicView.STYLESHEETS).build();
 
         final Stage stage = StageBuilder.create().style(StageStyle.UTILITY).title(title).build();
         stage.initModality(Modality.APPLICATION_MODAL);
@@ -25,15 +23,19 @@ public class InfoBox {
         final Label label = new Label(labelText);
         stage.setWidth(width);
         stage.setHeight(height);
-        final TextArea area = new TextArea(textAreaText);
-        area.setFocusTraversable(false);
-        area.setEditable(false);
+        TextArea area = null;
+        if (textAreaText != null) {
+            area = new TextArea(textAreaText);
+            area.setFocusTraversable(false);
+            area.setEditable(false);
+            VBox.setMargin(area, new Insets(5, 5, 0, 5));
+            VBox.setVgrow(area, Priority.ALWAYS);
+        }
         final Button close = new Button("Close");
         VBox.setMargin(label, new Insets(5, 5, 0, 5));
-        VBox.setMargin(area, new Insets(5, 5, 0, 5));
+
         VBox.setMargin(close, new Insets(5, 5, 5, 5));
 
-        VBox.setVgrow(area, Priority.ALWAYS);
         pane.setAlignment(Pos.CENTER);
 
         close.setDefaultButton(true);
@@ -42,13 +44,21 @@ public class InfoBox {
                 stage.close();
             }
         });
-        pane.getChildren().addAll(label, area, close);
+        if (area != null) {
+            pane.getChildren().addAll(label, area, close);
+        } else {
+            pane.getChildren().addAll(label, close);
+        }
 
         stage.show();
     }
 
     public static InfoBox make(final String title, final String labelText, final String textAreaText) {
-        return new InfoBox(title, labelText, textAreaText);
+        return make(title, labelText, textAreaText, 700, 600);
+    }
+
+    public static InfoBox make(final String title, final String labelText, final String textAreaText, final int width, final int height) {
+        return new InfoBox(title, labelText, textAreaText, width, height);
     }
 
 }
