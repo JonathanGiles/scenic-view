cc07a235cfc6ed9efbecce4f57519738f85ffd08
anderruiz <devnull@localhost>
2012-10-25 12:13:14 +0200

    Include a version for connector

--- src/com/javafx/experiments/fxconnector/remote/FXConnector.java
+++ src/com/javafx/experiments/fxconnector/remote/FXConnector.java
@@ -37,6 +37,8 @@ import com.javafx.experiments.fxconnector.AppController;
 
 public interface FXConnector {
 
+    public static final String VERSION = "1.0.0";
+
     List<AppController> connect();
 
     void close();
