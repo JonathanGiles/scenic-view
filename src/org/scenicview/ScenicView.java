5e67ea3d91a3cb7143d8bed738de3a52f88d47dd
Jonathan Giles <jonathan@jonathangiles.net>
2013-01-11 14:18:19 +1300

    Updating reference to stylesheet and the version number.

--- src/org/scenicview/ScenicView.java
+++ src/org/scenicview/ScenicView.java
@@ -103,10 +103,10 @@ import org.scenicview.details.GDetailPane.RemotePropertySetter;
 public class ScenicView extends Region implements ConnectorController, CParent {
 
     private static final String HELP_URL = "http://fxexperience.com/scenic-view/help";
-    public static final String STYLESHEETS = "com/javafx/experiments/scenicview/scenicview.css";
+    public static final String STYLESHEETS = ScenicView.class.getResource("scenicview.css").toExternalForm();
     public static final Image APP_ICON = DisplayUtils.getUIImage("mglass.png");
 
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "8.0.0 (Developer Preview 1)";
     // the Stage used to show Scenic View
     private final Stage scenicViewStage;
 
