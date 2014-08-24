fce0de7d2f93a48cf948bafd9497e109d4ab65a0
Jonathan Giles <jonathan@jonathangiles.net>
2014-08-25 10:33:52 +1200

    Comment out unused string.

--- src/org/fxconnector/remote/FXConnector.java
+++ src/org/fxconnector/remote/FXConnector.java
@@ -38,7 +38,7 @@ import org.fxconnector.AppController;
 public interface FXConnector {
 
     public static final String VERSION = "1.0.0";
-    public static final String SCENIC_VIEW_VM = "scenicview.vm";
+//    public static final String SCENIC_VIEW_VM = "scenicview.vm";
 
     List<AppController> connect();
 
