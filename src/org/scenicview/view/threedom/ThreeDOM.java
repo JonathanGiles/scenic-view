ac5f0520439afddcbd64c7bd5519e3e67c25369b
insidefx <nouard.arnaud@gmail.com>
2014-10-06 23:20:42 +0200

    Fix file headers

--- src/org/scenicview/view/threedom/ThreeDOM.java
+++ src/org/scenicview/view/threedom/ThreeDOM.java
@@ -1,7 +1,20 @@
 /*
- * ThreeDOM, 
- * Copyright (C) 2014 Arnaud Nouard
-*/ 
+ * Scenic View, 
+ * Copyright (C) 2014 Jonathan Giles, Ander Ruiz, Amy Fowler, Arnaud Nouard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.scenicview.view.threedom;
 
 import java.util.List;
@@ -45,6 +58,8 @@ import org.fxconnector.node.SVNode;
 
 /**
  * 2D to 3D
+ * Miss: a snapshot parameter to only capture container and not its descendant
+ * Mapping on only one face of 3D
  */
 public class ThreeDOM implements ITile3DListener {
 
